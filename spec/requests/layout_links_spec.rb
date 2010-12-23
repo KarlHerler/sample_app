43ea7c4cc9ab5411c9622d1abd65393da296c0c1
Karl Herler <hi@karlherler.com>
2010-12-23 15:05:30 +0200

    Finished layout and routes

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+  
+  it "should have a home page at '/'" do 
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+  
+  it "should have a Contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+  
+  it "should have a Help page at '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "Help")
+  end
+  
+  it "should have a a signup page at /signup" do 
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
+end
