23eacec6849c413f3060ea4619ea376bfa326e7e
Karl Herler <hi@karlherler.com>
2011-01-03 16:31:48 +0200

    User signupt complete

--- config/application.rb
+++ config/application.rb
@@ -46,5 +46,8 @@ module SampleApp
         ActiveSupport::Dependencies.mechanism = :load
       end
     end
+    
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
   end
 end
